1e850aa7b400096ad76ca4ce8e66c9c79ecfa638
Juuso Turunen <juusoturunen94@hotmail.com>
2016-01-29 20:16:06 +0200

    Unity Project

---
+++ Ritual/Assets/MovementScript.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MovementScript : MonoBehaviour
+{
+	// Use this for initialization
+	void Start ()
+    {
+	    
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        float movementSpeed = 10.0f;
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.Translate(Vector3.forward * Time.deltaTime * movementSpeed);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.Translate(Vector3.back * Time.deltaTime * movementSpeed);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(Vector3.right * Time.deltaTime * movementSpeed);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Translate(Vector3.left * Time.deltaTime * movementSpeed);
+        }
+	}
+}
---
+++ Ritual/Assets/PositionScript.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PositionScript : MonoBehaviour
+{
+    public GameObject player;
+
+	// Use this for initialization
+	void Start ()
+    {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        Vector3 relativePosition = new Vector3(0.0f, 10.0f, -10.0f);
+        transform.position = player.transform.position + relativePosition;
+	}
+}
