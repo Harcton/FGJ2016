fdf02c4131c5c5afd5176020c56cdfbd17da2d47
Juuso Turunen <juusoturunen94@hotmail.com>
2016-01-29 21:17:13 +0200

    Sprite added to player

--- Ritual/Assets/MovementScript.cs
+++ Ritual/Assets/MovementScript.cs
@@ -14,25 +14,4 @@ public class MovementScript : MonoBehaviour
     {
 
 	}
-
-    void handleInput()
-    {
-        float movementSpeed = 10.0f;
-        if (Input.GetKey(KeyCode.W))
-        {
-            transform.Translate(Vector3.forward * Time.deltaTime * movementSpeed);
-        }
-        if (Input.GetKey(KeyCode.S))
-        {
-            transform.Translate(Vector3.back * Time.deltaTime * movementSpeed);
-        }
-        if (Input.GetKey(KeyCode.D))
-        {
-            transform.Translate(Vector3.right * Time.deltaTime * movementSpeed);
-        }
-        if (Input.GetKey(KeyCode.A))
-        {
-            transform.Translate(Vector3.left * Time.deltaTime * movementSpeed);
-        }
-    }
 }
---
+++ Ritual/Assets/PlayerMovement.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public GameObject playerObject;
+    public float movementSpeed = 10.0f;
+
+	// Use this for initialization
+	void Start ()
+    {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        handlePlayerInput();
+	}
+
+    void handlePlayerInput()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            playerObject.GetComponent<Transform>().position += Vector3.forward * Time.deltaTime * movementSpeed;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            playerObject.GetComponent<Transform>().position += Vector3.back * Time.deltaTime * movementSpeed;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            playerObject.GetComponent<Transform>().position += Vector3.right * Time.deltaTime * movementSpeed;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            playerObject.GetComponent<Transform>().position += Vector3.left * Time.deltaTime * movementSpeed;
+        }
+    }
+}
