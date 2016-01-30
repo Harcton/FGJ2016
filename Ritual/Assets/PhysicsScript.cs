5ed2c46661ef74f443a070b5057e421fe24e43f3
Juuso Turunen <juusoturunen94@hotmail.com>
2016-01-30 02:18:36 +0200

    animations & collisions

---
+++ Ritual/Assets/PhysicsScript.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PhysicsScript : MonoBehaviour
+{
+    void OnTriggerStay (Collider col)
+    {
+        if (col.gameObject.name == "Tree")
+        {
+
+        }
+    }
+}
--- Ritual/Assets/PlayerMovement.cs
+++ Ritual/Assets/PlayerMovement.cs
@@ -4,9 +4,21 @@ using System.Collections;
 public class PlayerMovement : MonoBehaviour
 {
     public GameObject playerObject;
-    public float movementSpeed = 10.0f;
+    public float movementSpeed = 200.0f;
 
     private Animator playerAnimator;
+    private bool facingRight = false;
+    private float idleTime = 0.0f;
+
+    enum AnimationState
+    {
+        Standing = 0,
+        Walking = 1,
+        Gangsta = 2,
+        Buddha = 3,
+        Fire = 4,
+        Drinking = 5,
+    }
 
 	// Use this for initialization
 	void Start ()
@@ -17,6 +29,7 @@ public class PlayerMovement : MonoBehaviour
 	// Update is called once per frame
 	void Update ()
     {
+        idleTime += Time.deltaTime;
         handlePlayerInput();
 	}
 
@@ -24,31 +37,76 @@ public class PlayerMovement : MonoBehaviour
     {
         if (Input.GetKey(KeyCode.W))
         {
+            idleTime = 0.0f;
             playerObject.GetComponent<Transform>().position += Vector3.forward * Time.deltaTime * movementSpeed;
-            playerAnimator.SetInteger("State", 1);
+            playerAnimator.SetInteger("State", (int)AnimationState.Walking);
         }
         if (Input.GetKey(KeyCode.S))
         {
+            idleTime = 0.0f;
             playerObject.GetComponent<Transform>().position += Vector3.back * Time.deltaTime * movementSpeed;
-            playerAnimator.SetInteger("State", 1);
+            playerAnimator.SetInteger("State", (int)AnimationState.Walking);
         }
         if (Input.GetKey(KeyCode.D))
         {
+            idleTime = 0.0f;
             playerObject.GetComponent<Transform>().position += Vector3.right * Time.deltaTime * movementSpeed;
-            playerAnimator.SetInteger("State", 1);
+            if (!facingRight)
+            {
+                FlipPlayer();
+            }
+            facingRight = true;
+            playerAnimator.SetInteger("State", (int)AnimationState.Walking);
         }
         if (Input.GetKey(KeyCode.A))
         {
+            idleTime = 0.0f;
             playerObject.GetComponent<Transform>().position += Vector3.left * Time.deltaTime * movementSpeed;
-            playerAnimator.SetInteger("State", 1);
+            if (facingRight)
+            {
+                FlipPlayer();
+            }
+            facingRight = false;
+            playerAnimator.SetInteger("State", (int)AnimationState.Walking);
         }
-        if (Input.GetKey(KeyCode.Space))
+
+        if (Input.GetKey(KeyCode.Tab))
         {
-            playerAnimator.SetInteger("State", 2);
+            idleTime = 0.0f;
+            playerAnimator.SetInteger("State", (int)AnimationState.Fire);
+        }
+        else if (Input.GetKey(KeyCode.Return))
+        {
+            idleTime = 0.0f;
+            playerAnimator.SetInteger("State", (int)AnimationState.Buddha);
+        }
+        else if (Input.GetKey(KeyCode.Space))
+        {
+            idleTime = 0.0f;
+            playerAnimator.SetInteger("State", (int)AnimationState.Gangsta);
         }
         else if (!Input.GetKey(KeyCode.A) && !Input.GetKey(KeyCode.S) && !Input.GetKey(KeyCode.D) && !Input.GetKey(KeyCode.W))
         {
-            playerAnimator.SetInteger("State", 0);
+            if (idleTime > 3.0f)
+            {
+                playerAnimator.SetInteger("State", (int)AnimationState.Drinking);
+                if (idleTime > 4.0f)
+                {
+                    idleTime = 0.0f;
+                }
+            }
+            else
+            {
+                playerAnimator.SetInteger("State", (int)AnimationState.Standing);
+            }
         }
+    }
+
+    void FlipPlayer()
+    {
+        facingRight = !facingRight;
+        Vector3 scale = playerObject.GetComponent<Transform>().localScale;
+        scale.x *= -1;
+        playerObject.GetComponent<Transform>().localScale = scale;
     }
 }
